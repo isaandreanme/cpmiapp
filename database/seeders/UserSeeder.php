50bed590345e26c833cbe1e3ecb3b16f684ad3eb
zimamarrois <zimamarrois@gmail.com>
2024-06-03 17:13:11 +0000

    MySQL IDX

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,8 +16,8 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'Super Admin',
-            'email' => 'superadmin@bitewild.com',
-            'password' => Hash::make('Passwords'),
+            'email' => 'superadmin@gmail.com.com',
+            'password' => Hash::make('password'),
             'email_verified_at' => '2023-01-01 00:00:00'
         ]);
     }
