1b3aafe092f5e788e95213a0a850db3b72226e4c
zimamarrois <zimamarrois@gmail.com>
2024-06-03 16:25:36 +0000

    idx

---
+++ .idx/dev.nix
@@ -0,0 +1,29 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.php82
+    pkgs.php82Packages.composer
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
--- app/Policies/BiodataPolicy.php
+++ app/Policies/BiodataPolicy.php
@@ -3,10 +3,10 @@
 namespace App\Policies;
 
 use App\Models\User;
-use App\Models\Agency;
+use App\Models\Biodata;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class AgencyPolicy
+class BiodataPolicy
 {
     use HandlesAuthorization;
 
@@ -15,15 +15,15 @@ class AgencyPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->can('view_any_agency');
+        return $user->can('view_any_biodata');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Agency $agency): bool
+    public function view(User $user, Biodata $biodata): bool
     {
-        return $user->can('view_agency');
+        return $user->can('view_biodata');
     }
 
     /**
@@ -31,23 +31,23 @@ class AgencyPolicy
      */
     public function create(User $user): bool
     {
-        return $user->can('create_agency');
+        return $user->can('create_biodata');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Agency $agency): bool
+    public function update(User $user, Biodata $biodata): bool
     {
-        return $user->can('update_agency');
+        return $user->can('update_biodata');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Agency $agency): bool
+    public function delete(User $user, Biodata $biodata): bool
     {
-        return $user->can('delete_agency');
+        return $user->can('delete_biodata');
     }
 
     /**
@@ -55,15 +55,15 @@ class AgencyPolicy
      */
     public function deleteAny(User $user): bool
     {
-        return $user->can('delete_any_agency');
+        return $user->can('delete_any_biodata');
     }
 
     /**
      * Determine whether the user can permanently delete.
      */
-    public function forceDelete(User $user, Agency $agency): bool
+    public function forceDelete(User $user, Biodata $biodata): bool
     {
-        return $user->can('force_delete_agency');
+        return $user->can('force_delete_biodata');
     }
 
     /**
@@ -71,15 +71,15 @@ class AgencyPolicy
      */
     public function forceDeleteAny(User $user): bool
     {
-        return $user->can('force_delete_any_agency');
+        return $user->can('force_delete_any_biodata');
     }
 
     /**
      * Determine whether the user can restore.
      */
-    public function restore(User $user, Agency $agency): bool
+    public function restore(User $user, Biodata $biodata): bool
     {
-        return $user->can('restore_agency');
+        return $user->can('restore_biodata');
     }
 
     /**
@@ -87,15 +87,15 @@ class AgencyPolicy
      */
     public function restoreAny(User $user): bool
     {
-        return $user->can('restore_any_agency');
+        return $user->can('restore_any_biodata');
     }
 
     /**
      * Determine whether the user can replicate.
      */
-    public function replicate(User $user, Agency $agency): bool
+    public function replicate(User $user, Biodata $biodata): bool
     {
-        return $user->can('replicate_agency');
+        return $user->can('replicate_biodata');
     }
 
     /**
@@ -103,6 +103,6 @@ class AgencyPolicy
      */
     public function reorder(User $user): bool
     {
-        return $user->can('reorder_agency');
+        return $user->can('reorder_biodata');
     }
 }
