df4edfe352cc36e5ef959992f83819a89f2f6b73
zimamarrois <zimamarrois@gmail.com>
2024-06-05 13:03:25 +0000

    Update Biodata Realasi

--- app/Filament/Resources/BiodataResource/Pages/EditBiodata.php
+++ app/Filament/Resources/BiodataResource/Pages/EditBiodata.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\BiodataResource\Pages;
 
 use App\Filament\Resources\BiodataResource;
+use App\Models\User;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
+use Filament\Notifications\Actions\Action as NotificationAction; // Aliaskan tipe Action dari Filament\Notifications\Actions
 
 class EditBiodata extends EditRecord
 {
@@ -20,5 +23,37 @@ class EditBiodata extends EditRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        $data = $this->record;
+
+        // Buat tombol "View" dengan tipe yang benar
+        $viewButton = NotificationAction::make('Lihat')
+            ->url(BiodataResource::getUrl('view', ['record' => $data]));
+
+        $recipients = User::all();
+
+        foreach ($recipients as $recipient) {
+
+            $editorName = auth()->user()->name; // Menggunakan Auth untuk mendapatkan nama pengguna yang sedang masuk
+
+            // Buat notifikasi dengan tombol "View"
+            $notification = Notification::make()
+                ->title('PENDAFTARAN')
+                // ->body('Data CPMI Berhasil Diubah')
+                ->body("<strong>{$data->nama}</strong> Berhasil Update
+                <br>
+                Oleh <strong>{$editorName}</strong>") 
+                ->actions([$viewButton]) // Tambahkan tombol "View" ke notifikasi
+                // ->send()
+                ->persistent()
+                ->success()
+                ->duration(1000)
+                ->sendToDatabase($recipient);
+        }
+
+        return $notification;
     }
 }
